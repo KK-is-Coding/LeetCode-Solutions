d2bfb76576fb6301a968a710469c9fe2390d9bfe
KK-is-Coding <krishnakesarwani8081@gmail.com>
2023-03-19 22:46:26 +0530

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ 1491.py
@@ -0,0 +1,17 @@
+class Solution:
+    def average(self, salary):
+        salary.sort()
+        i = 1
+        sum = 0
+        while (i < len(salary)-1):
+            sum += salary[i]
+            i += 1
+
+        avg = sum/(len(salary)-2)
+        return avg
+
+
+salary = list(map(int, input().split(" ")))
+sol = Solution
+out = sol.average(sol, salary)
+print(out)
